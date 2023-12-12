2c8614077396386c34c0455eea79a4118519b9c5
jhchoi1182 <jhchoi1182@gmail.com>
2023-12-12 17:35:04 +0900

    todo 추가기능 및 유효성 검사

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,11 +1,17 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("name")
@@ -18,12 +24,30 @@ public class TodoController {
 		this.todoService = todoService;
 	}
 
-
-
 	@RequestMapping("list-todos")
 	public String listAllTodos(ModelMap model) {
 		List<Todo> todos = todoService.findByUsername("in28minutes");
 		model.addAttribute("todos", todos);
 		return "listTodos";
+	}
+	
+	@RequestMapping(value="add-todo", method = RequestMethod.GET)
+	public String showNewTodoPage(ModelMap model) {
+		String username = (String)model.get("name");
+		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+		model.put("todo", todo);	// todo.jsp의 modelAttribute와 일치시키기
+		return "todo";
+	}
+	
+	@RequestMapping(value="add-todo", method = RequestMethod.POST)
+	public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {	// todo.jsp의 modelAttribute와 일치시키기
+		if(result.hasErrors()) {
+			return "todo";
+		}
+		
+		String username = (String)model.get("name");
+		
+		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
+		return "redirect:list-todos";
 	}
 }
