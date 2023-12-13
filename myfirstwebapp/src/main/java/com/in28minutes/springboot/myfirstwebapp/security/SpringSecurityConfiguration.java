c02f92c42174dcf08b01c378395eae5a3ff2ea02
jhchoi1182 <jhchoi1182@gmail.com>
2023-12-13 22:58:00 +0900

    유저 추가

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -16,16 +16,22 @@ public class SpringSecurityConfiguration {
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
 		
+		UserDetails userDetails1 = createNewUser("jhchoi1182", "321321");
+		UserDetails userDetails2 = createNewUser("yhhnnmm", "321321");
+		
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+	}
+
+	private UserDetails createNewUser(String username, String password) {
 		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
 		
 		UserDetails userDetails = User.builder()
 									.passwordEncoder(passwordEncoder)
-									.username("jhchoi1182")
-									.password("321321")
+									.username(username)
+									.password(password)
 									.roles("USER", "ADMIN")
 									.build();
-		
-		return new InMemoryUserDetailsManager(userDetails);
+		return userDetails;
 	}
 	
 	@Bean
