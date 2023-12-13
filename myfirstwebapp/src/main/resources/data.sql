dac26ffc6bc6bd3cfda57cefcf849a741098180e
jhchoi1182 <jhchoi1182@gmail.com>
2023-12-13 23:13:21 +0900

    H2 연결 설정

---
+++ myfirstwebapp/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'yhhnnmm', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'yhhnnmm', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'yhhnnmm', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'yhhnnmm', 'Learn DevOps', CURRENT_DATE(), false);
