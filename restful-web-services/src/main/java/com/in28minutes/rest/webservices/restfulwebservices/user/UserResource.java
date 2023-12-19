45aee04a97cf993fe1df48d8812da43e4d0dc993
jhchoi1182 <jhchoi1182@gmail.com>
2023-12-19 23:55:40 +0900

    REST API:POST 메소드 추가

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,9 +1,16 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -19,4 +26,24 @@ public class UserResource {
 		return service.findAll();
 	}
 	
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()	// 현재 요청에 해당하는 URL를 반환
+						.path("/{id}")	// URL에 추가
+						.buildAndExpand(savedUser.getId())	// id 부분은 바꿔줌
+						.toUri();   // 값을 URI로 변경
+		
+		// location대신 null을 입력하면 응답 헤더에 아무것도 안 실림
+		// location을 지정해줄 경우 생성된 리소스의 uri값을 응답값 헤더에 실어서 반환할 수 있음
+		// ex) http://localhost:8080/users/4
+		return ResponseEntity.created(location).build();
+	}
 }
