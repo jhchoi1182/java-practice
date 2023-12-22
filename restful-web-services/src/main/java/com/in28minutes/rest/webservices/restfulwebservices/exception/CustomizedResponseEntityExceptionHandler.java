ad69c3285b699680433c2509448c08bd8810d000
jhchoi1182 <jhchoi1182@gmail.com>
2023-12-22 18:58:10 +0900

    REST API:유효성 검사 추가

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.in28minutes.rest.webservices.restfulwebservices.exception;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -31,4 +35,22 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
 	}
 	
+	// ResponseEntityExceptionHandler에 있는 handleMethodArgumentNotValid를 오버라이드해서 커스텀
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+		// 1. 기본 메세지
+		String defaultMessage = ex.getMessage();
+		// 2. 내가 설정한 메세지만 보여주고 싶을 때
+		String customMessage = ex.getFieldError().getDefaultMessage();
+		// 3. 2번에서 에러가 여러 개일 수 있는 경우
+		String customMessages = ex.getFieldErrors().stream()
+							        .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+							        .collect(Collectors.joining(", "));
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+	}
+
 }
