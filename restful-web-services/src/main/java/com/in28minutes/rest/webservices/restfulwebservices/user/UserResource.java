80ccf849596f06efa0c2ae3d6baf248466b9f72c
jhchoi1182 <jhchoi1182@gmail.com>
2023-12-26 22:08:11 +0900

    REST API:HATEOAS 적용

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,14 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+// import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*; 도 가능
+
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,13 +36,23 @@ public class UserResource {
 	}
 	
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	// EntityModel 설정
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
 		
 		if(user==null)
 			throw new UserNotFoundException("id:"+id);
 		
-		return user;
+		// EntityModel 클래스 인스턴스 생성
+		EntityModel<User> entityModel = EntityModel.of(user);
+		
+		// WebMvcLinkBuilder - 스프링 MVC 컨트롤러를 가리키는 링크 인스턴스 구축을 용이하게 해주는 빌더
+		// EntityModel에 WebMvcLinkBuilder를 이용해 retrieveAllUsers 링크 추가
+		WebMvcLinkBuilder link =  linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		// 링크 이름 지정
+		entityModel.add(link.withRel("all-users"));
+		
+		return entityModel;
 	}
 	
 	@PostMapping("/users")
